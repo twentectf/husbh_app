81e36e1667842cc12be95aee5182637dbaa8f12d
Mimich6n <433201380@student.ksu.edu.sa>
2022-05-13 18:41:10 +0300

    result page editing

--- lib/screens/subtraction/subtractionResultScreen.dart
+++ lib/screens/subtraction/subtractionResultScreen.dart
@@ -73,7 +73,7 @@ class subtractionResultScreen extends StatelessWidget {
           children: [
             Center(
               child: Container(
-                height: height * 0.70,
+                height: height * 0.75,
                 width: width * 0.35,
                 //color: Colors.white,
                 //blur: 0,
